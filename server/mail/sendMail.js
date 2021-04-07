7e653770cb2ff0f840396447c78ea00623070dd0
sverissimo <sandroverissimo@live.com>
2021-04-07 19:22:51 -0300

    Identificação de problemas de log no backEnd do servidor: chamadas constantes de outros endPoints poluem e distorcem o monitoramento do servidor. Pesquisa e tracing de IP solucionar o problema. Configuração do servidor e do smtp gmail com alias para o envio de e-mail sem cair na caixa de spam. Configuração de TLS em porta 465 e de remetente para renderização como "Seinfra - CadTI". Criação de servidor http (direto no servidor) na porta 3002 e de novo proxy reverso (nginx) para mock (simulação) de testes de envio automático de e-mail. Criação de template html estático para a formatação básica do corpo do e-mail.

--- server/mail/sendMail.js
+++ server/mail/sendMail.js
@@ -1,12 +1,38 @@
-const sendmail = require('sendmail')();
- 
-sendmail({
-    from: 'alertas@cadti.mg.gov.br',
-	//from: 'cadti@infraestrutura.mg.gov.br',
-    to: 'sverissimo2@gmail.com',
-    subject: 'New stuff, man',
-    html: 'stuf... done!',
-  }, function(err, reply) {
-    console.log(err && err.stack);
-    console.dir(reply);
-});
+const nodemailer = require("nodemailer")
+const fs = require('fs')
+
+async function main() {
+
+  const x = fs.readFileSync('./seguroExpiring.html', (err, doc) => {
+    console.log(err)
+  })
+  const htmlMail = x && x.toString()
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'seinframg20@gmail.com', // generated ethereal user
+      pass: '!!s3iNfr@', // generated ethereal password
+    }
+  })
+
+  let info = await transporter.sendMail({
+    from: 'CadTI', // sender address
+    //to: 'sandroverissimo@live.com',
+    //to: 'sverissimo2@gmail.com',
+    to: "sandro.verissimo@infraestrutura.mg.gov.br", // list of receivers
+    subject: "Whatup, svom!!!??", // Subject line    
+    html: htmlMail // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
