a0c711a3d7d9c635198884a5f0fc0788e278c2b9
Sandro Veríssimo <sandroverissimo@live.com>
2022-11-30 15:11:30 -0300

    Instalada e configurada dependência de testes (jest) no backend.

---
+++ server/tests/seguros.test.js
@@ -0,0 +1,15 @@
+describe('Seguros -> listing all insurances', () => {
+    it('Should return at least one record with all SeguroModel properties', () => {
+
+        console.log('To be implemented...')
+    })
+
+})
+
+describe('Seguros -> cadastrando um novo seguro', () => {
+    it('Should save the passed object to the test DB', () => {
+
+        console.log('To be implemented...')
+    })
+
+})
