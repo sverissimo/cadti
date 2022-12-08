6b2d7e9852153008c9fec501e3ce3638946fcd80
Sandro Veríssimo <sandroverissimo@live.com>
2022-12-07 22:07:35 -0300

    Separação entre app.js e server.js

---
+++ server/app.js
@@ -0,0 +1,41 @@
+//@ts-check
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const { setCorsHeader } = require('./config/setCorsHeader')
+const router = require('./routes')
+const dbSync = require('./sync/dbSyncAPI')
+const authRouter = require('./auth/authRouter')
+const authToken = require('./auth/authToken')
+const taskManager = require('./taskManager/taskManager')
+const errorHandler = require('./utils/errorHandler')
+const { fileRouter: useFileRouter } = require('./routes/fileRoutes')
+
+taskManager()
+dotenv.config()
+
+const app = express()
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('client/build'))
+app.use(setCorsHeader)
+app.use(morgan('dev'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(morgan(':method :url :status :response-time ms [:date]'))
+}
+app.use('/auth', authRouter)
+app.use(authToken)
+app.use('/api', router)
+app.use('/sync', dbSync)
+useFileRouter(app)
+
+if (process.env.NODE_ENV === 'production') {
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    })
+}
+app.use(...errorHandler)
+
+module.exports = app
--- server/server.js
+++ server/server.js
@@ -1,77 +1,24 @@
-const
-    fs = require('fs'),
-    express = require('express'),
-    httpsOptions = {
+//@ts-check
+const fs = require('fs')
+const https = require('https')
+const http = require('http')
+const app = require('./app')
+const { createSocketConnection } = require('./sockets/createSocketConnection')
+
+const port = process.env.PORT || 3001
+let server
+
+if (process.env.NODE_ENV === 'production') {
+    const httpsOptions = {
         key: fs.readFileSync("/sismob/certificates/localhost.key"),
         cert: fs.readFileSync("/sismob/certificates/localhost.crt"),
-    },
-    app = express(),
-    productionServer = require('https').createServer(httpsOptions, app),
-    devServer = require('http').createServer(app),
-    path = require('path'),
-    dotenv = require('dotenv'),
-    mongoose = require('mongoose'),
-    morgan = require('morgan'),
-    Grid = require('gridfs-stream')
-Grid.mongo = mongoose.mongo
-
-//Componentes do sistema
-const
-    { setCorsHeader } = require('./config/setCorsHeader'),
-    router = require('./routes'),
-    { createSocketConnection } = require('./sockets/createSocketConnection'),
-    dbSync = require('./sync/dbSyncAPI'),
-    authRouter = require('./auth/authRouter'),
-    authToken = require('./auth/authToken'),
-    taskManager = require('./taskManager/taskManager'),
-    errorHandler = require('./utils/errorHandler'),
-    { fileRouter: useFileRouter } = require('./routes/fileRoutes')
-
-
-taskManager()
-dotenv.config()
-
-app.use(express.json({ limit: '50mb' }))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('client/build'))
-app.use(setCorsHeader)
-app.use(morgan('dev'))
-if (process.env.NODE_ENV === 'production') {
-    app.use(morgan(':method :url :status :response-time ms [:date]'))
-}
-
-app.use('/auth', authRouter)
-app.use(authToken)
-
-let server
-if (process.env.NODE_ENV === 'development') {
-    server = devServer
-    console.log('Socket listening to devServer...')
-}
-else {
-    server = productionServer
-    console.log('Socket listening to https server...')
+    }
+    server = https.createServer(httpsOptions, app)
+} else {
+    server = http.createServer(app)
 }
 
 const io = createSocketConnection(app, server)
 app.set('io', io)
 
-useFileRouter(app)
-app.use('/api', router)
-app.use('/sync', dbSync)
-
-if (process.env.NODE_ENV === 'production') {
-    app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
-    })
-}
-
-app.use(...errorHandler)
-
-if (require.main === module) {
-    server.listen(process.env.PORT || 3001, () => {
-        console.info('NodeJS server running on port ' + (process.env.PORT || 3001))
-    })
-}
-
-module.exports = app
+server.listen(port, () => console.info(`NodeJS server running on port ${port}...`))
