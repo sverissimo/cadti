e4168b49c2ad858b68e40c86d3aeb56b0a7d5671
Sandro Veríssimo <sandroverissimo@live.com>
2023-01-23 18:39:11 -0300

    Atualização do webSocketsClient.js (frontEnd): remoção de listen de eventos removidos no backEnd.

--- client/src/Store/webSocketsClient.js
+++ client/src/Store/webSocketsClient.js
@@ -6,35 +6,24 @@ const { webSocketHost, options } = getEnvironment()
 
 const startSocket = ({ insertData, updateData, updateCollection, deleteOne, updateDocs, user, editUser }) => {
     let socket
-
-    if (!socket)
+    if (!socket) {
         socket = socketIO({ url: webSocketHost, options })
+    }
 
-    //Conecta o usuário em um socket, passando suas informações
     socket.on('connect', () => socket.emit('userDetails', user))
 
-    //********************Listen to socket events and call dataActions*********************** */
-    //REFACTOR!!!!!!!!
-    socket.on('insertVehicle', insertedObjects => insertData(insertedObjects, 'veiculos'))
     socket.on('insertElements', ({ data, collection }) => insertData(data, collection))
     socket.on('addElements', ({ insertedObjects, table }) => {
         const { collection } = configVehicleForm.find(el => el.table === table)
         insertData(insertedObjects, collection)
     })
-
-    socket.on('updateVehicle', updatedObjects => updateData(updatedObjects, 'veiculos', 'veiculoId'))
-    socket.on('updateInsurance', updatedObjects => updateCollection(updatedObjects, 'seguros'))
-    socket.on('updateLogs', updatedObjects => updateData(updatedObjects, 'logs', 'id'))
     socket.on('updateAny', ({ data, collection, primaryKey }) => updateData(data, collection, primaryKey))
     socket.on('updateDocs', ({ ids, metadata, collection, primaryKey }) => updateDocs(ids, metadata, collection, primaryKey))
-    socket.on('updateElements', ({ collection, updatedCollection }) => updateCollection(updatedCollection, collection))
     socket.on('updateUser', updatedUser => editUser(updatedUser))
-
     socket.on('deleteOne', ({ id, tablePK, collection }) => {
         console.log({ id, tablePK, collection })
         deleteOne(id, tablePK, collection)
     })
-
     socket.on('insertFiles', object => {
         const { insertedObjects, collection } = object
         insertData(insertedObjects, collection)
